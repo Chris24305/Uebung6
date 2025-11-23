ddd70e2300c392063fb18ff26a8bb7e2b0976822
Chris24305 <christian.schaa@web.de>
2025-11-23 15:09:00 +0100

    H2V1

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -2,10 +2,26 @@ package h2;
 
 public class H2_main {
 	public static void main(String[] args) {
-		
+		int[] a = { 1, 2, 3, 4 }, b = { 1, 2, 3, 5 };
+		boolean identical = compareArrays(a, b);
+		System.out.print(identical);
 	}
-	public static boolean compareArrays (int[] A, int[] B) {
-		
-		return false;
+
+	public static boolean compareArrays(int[] A, int[] B) {
+		boolean compareArrays = false;
+		if (A.length != B.length) {
+			compareArrays = false;
+		} else {
+			for (int m = 0; m < A.length; m++) {
+				if (A[m] == B[m]) {
+					compareArrays = true;
+					continue;
+				} else {
+					compareArrays = false;
+					break;
+				}
+			}
+		}
+		return compareArrays;
 	}
 }

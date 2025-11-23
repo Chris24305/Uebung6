f683e9563970fb7a33c1fa2846ddea6e032574db
Chris24305 <christian.schaa@web.de>
2025-11-23 14:41:38 +0100

    addedComment

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,11 +2,12 @@ package h1;
 
 public class H1_main {
 	public static void main(String[] args) {
-		int[] e = { 7,6,5,3,2,1 };
-		int[] d = { 1,2,3,5,6,7 };
-		boolean c=isMirrorArray(e,d);
-		System.out.println(c);
+	  /*int[] c = { 7, 6, 5, 4, 3, 2, 1 };
+		int[] d = { 1, 2, 3, 4, 5, 6, 7 };
+		boolean e = isMirrorArray(c, d);
+		System.out.println(e)*/	
 	}
+
 	public static boolean isMirrorArray(int[] a, int[] b) {
 		boolean isMirrorArray = false;
 		for (int m = 0, n = 0; n < b.length && m < a.length; m++, n++) {
